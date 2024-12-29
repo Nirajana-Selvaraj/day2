7791f114d995b8ca805404e30b830706d55881c2
Nirajana-Selvaraj <nirajanasri2304@gmail.com>
2024-12-29 23:58:37 +0530

    day 3 tasks

---
+++ task3/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Class1 from './components/class/class1';
+
+function App() {
+ 
+
+  return (
+   <div>
+      <Class1/>
+    </div>
+      
+    
+  );
+}
+
+export default App;
+  
